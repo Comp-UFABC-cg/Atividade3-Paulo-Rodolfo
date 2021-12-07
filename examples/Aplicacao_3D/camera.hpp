8d78e2ae4229dc1a265aeceb5fc3a156602c6760
Paulo Simoes <paulo.alexander@aluno.ufabc.edu.br>
2021-12-07 00:15:47 -0300

    Acerto posição Camera

--- examples/Aplicacao_3D/camera.hpp
+++ examples/Aplicacao_3D/camera.hpp
@@ -20,8 +20,8 @@ class Camera {
  private:
   friend OpenGLWindow;
 
-  glm::vec3 m_eye{glm::vec3(0.0f, 0.5f, 2.5f)};  // Camera position
-  glm::vec3 m_at{glm::vec3(0.0f, 0.5f, 0.0f)};   // Look-at point
+  glm::vec3 m_eye{glm::vec3(0.0f, 0.0f, 2.5f)};  // Camera position
+  glm::vec3 m_at{glm::vec3(0.0f, 0.0f, 0.0f)};   // Look-at point
   glm::vec3 m_up{glm::vec3(0.0f, 1.0f, 0.0f)};   // "up" direction
   glm::vec3 m_horizontal{glm::vec3(1.0f, 0.0f, 0.0f)};   // "horizontal" direction
 
