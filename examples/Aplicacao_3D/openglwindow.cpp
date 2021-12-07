8fa1bc428be8a2ad5ae7afff6ac113a46ddb2692
Paulo Simoes <paulo.alexander@aluno.ufabc.edu.br>
2021-12-07 00:09:10 -0300

    Ajuste Camera

--- examples/Aplicacao_3D/openglwindow.cpp
+++ examples/Aplicacao_3D/openglwindow.cpp
@@ -122,54 +122,8 @@ void OpenGLWindow::initializeGL() {
   loadModel(getAssetsPath() + "cenario/medieval.obj");
   m_mappingMode = 3;  // "From mesh" option
 
-  // Initial trackball spin
-  // m_trackBallModel.setAxis(glm::normalize(glm::vec3(1, 1, 1)));
-  // m_trackBallModel.setVelocity(0.0001f);
-  
   // Trackballs -------------------------
-/*
-  // Create program
 
- 
-
-  // CHAMADA Load model INCLUIR
-
-  // Generate VBO
-  abcg::glGenBuffers(1, &m_VBO);
-  abcg::glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
-  abcg::glBufferData(GL_ARRAY_BUFFER, sizeof(m_vertices[0]) * m_vertices.size(),
-                     m_vertices.data(), GL_STATIC_DRAW);
-  abcg::glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-  // Generate EBO
-  abcg::glGenBuffers(1, &m_EBO);
-  abcg::glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
-  abcg::glBufferData(GL_ELEMENT_ARRAY_BUFFER,
-                     sizeof(m_indices[0]) * m_indices.size(), m_indices.data(),
-                     GL_STATIC_DRAW);
-  abcg::glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-  // Create VAO
-  abcg::glGenVertexArrays(1, &m_VAO);
-
-  // Bind vertex attributes to current VAO
-  abcg::glBindVertexArray(m_VAO);
-
-  abcg::glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
-  const GLint positionAttribute{
-      abcg::glGetAttribLocation(m_program, "inPosition")};
-  abcg::glEnableVertexAttribArray(positionAttribute);
-  abcg::glVertexAttribPointer(positionAttribute, 3, GL_FLOAT, GL_FALSE,
-                              sizeof(Vertex), nullptr);
-  abcg::glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-  abcg::glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
-
-  // End of binding to current VAO
-  abcg::glBindVertexArray(0);
-
-  resizeGL(getWindowSettings().width, getWindowSettings().height);
-  */
   }
   catch(std::exception& e)
   {
