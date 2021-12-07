b25c25e862d56bdba43f21c2943fd724d730fac5
Rodolfoazevedo <rodolfo_azevedo@live.com>
2021-12-06 22:06:30 -0300

    ajuste camera cpp

--- examples/Aplicacao_3D/camera.cpp
+++ examples/Aplicacao_3D/camera.cpp
@@ -4,7 +4,7 @@
 
 void Camera::computeProjectionMatrix(int width, int height) {
   m_projMatrix = glm::mat4(1.0f);
-  const auto aspect{static_cast<float>(width) / static_cast<float>(height)};
+  auto aspect{static_cast<float>(width) / static_cast<float>(height)};
   m_projMatrix = glm::perspective(glm::radians(70.0f), aspect, 0.1f, 5.0f);
 }
 
@@ -14,7 +14,7 @@ void Camera::computeViewMatrix() {
 
 void Camera::dolly(float speed) {
   // Compute forward vector (view direction)
-  const glm::vec3 forward{glm::normalize(m_at - m_eye)};
+  glm::vec3 forward{glm::normalize(m_at - m_eye)};
 
   // Move eye and center forward (speed > 0) or backward (speed < 0)
   m_eye += forward * speed;
@@ -25,9 +25,9 @@ void Camera::dolly(float speed) {
 
 void Camera::truck(float speed) {
   // Compute forward vector (view direction)
-  const glm::vec3 forward{glm::normalize(m_at - m_eye)};
+  glm::vec3 forward{glm::normalize(m_at - m_eye)};
   // Compute vector to the left
-  const glm::vec3 left{glm::cross(m_up, forward)};
+  glm::vec3 left{glm::cross(m_up, forward)};
 
   // Move eye and center to the left (speed < 0) or to the right (speed > 0)
   m_at -= left * speed;
